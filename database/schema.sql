c6579689ea75c33cc164edb7e64b35b6d97ef836
mateoraeth <85139120+mateoraeth@users.noreply.github.com>
2022-04-28 17:48:59 -0700

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,18 @@
+CREATE TABLE users(
+      id int PRIMARY KEY NOT NULL,
+      name VARCHAR(30) NOT NULL UNIQUE,
+      email VARCHAR(50) NOT NULL UNIQUE,
+      premium_id int);
 
+CREATE TABLE threads (
+      id int PRIMARY KEY NOT NULL,
+      theme VARCHAR(20) NOT NULL,
+      topic VARCHAR(50) NOT NULL,
+      creator_id int NOT NULL);
+
+CREATE TABLE messages (
+      id int NOT NULL,
+      thread_id int NOT NULL,
+      message VARCHAR(500) NOT NULL,
+      creator_id int NOT NULL,
+      PRIMARY KEY (id, thread_id));
