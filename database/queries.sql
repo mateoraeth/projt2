445a61d7c45c1dcffa6124f6eceef79eb25229ee
mateoraeth <85139120+mateoraeth@users.noreply.github.com>
2022-04-28 17:48:18 -0700

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,1 +1,3 @@
-
+SELECT users.name, messages.message FROM users JOIN messages ON users.id = messages.creator_id;
+SELECT messages.message FROM messages JOIN threads ON messages.thread_id = threads.id WHERE threads.theme LIKE 'Mov%';
+SELECT threads.topic FROM threads JOIN users ON users.id = threads.creator_id WHERE users.name = 'Mateo Raeth';
